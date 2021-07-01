e390b5e2ea865882ee9a1a9b5313c8fccea2a5bf
Afrin Momin <afrinmomin06@gmail.com>
2021-07-01 03:36:31 -0500

    design and code

---
+++ estee-lauder-challenge/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  estee-lauder-challenge
+//
+//  Created by Afrin Momin on 6/30/21.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgImage: UIImageView!
+}
--- estee-lauder-challenge/ViewController.swift
+++ estee-lauder-challenge/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var imageArray = [UIImage(named: "EL_18"), UIImage(named: "EL_20"), UIImage(named: "EL_22"), UIImage(named: "EL_24"), UIImage(named: "EL_26"), UIImage(named: "EL_27"), UIImage(named: "EL_28"), UIImage(named: "EL_29"),]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // imageArray
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCollectionViewCell", for: indexPath) as! ImageCollectionViewCell
 
-
+        cell.imgImage.image = imageArray[indexPath.row]
+        return cell
+    }
+    
 }
 
